e6bdbfe76fb00f71df65e67971ccfb35ec222553
Morgan J <morgan.jalil@medieinstitutet.se>
2019-05-24 11:37:46 +0200

    added portnumber

--- expressServer.js
+++ expressServer.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 // Setup the server
+const PORT = 3000;
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
